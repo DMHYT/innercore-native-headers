bba4727200b3133adb6d7c9ce60e882b862dda61
visualstudiodan <danmarkhunter13072006@gmail.com>
2023-08-31 02:58:37 +0300

    Added `biomes.h`

---
+++ horizon/innercore/biomes.h
@@ -0,0 +1,11 @@
+#ifndef INNER_CORE_BIOMES_H
+#define INNER_CORE_BIOMES_H
+
+
+namespace CustomBiomeRegistry {
+    int toRuntimeId(int virtualId);
+    int toVirtualId(int runtimeId);
+}
+
+
+#endif
